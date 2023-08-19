d91515c45db87eb09a1fcd17d833d17b99a1495a
dadsec-dev <geecypher41@gmail.com>
2023-08-19 11:20:01 +0100

    products page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+
 
 const App = () => {
   return (
-    <div>Welcome to LUxe-Market</div>
+    <>
+    <Header />
+
+    <main className='py-3'>
+      <Container>
+      <HomeScreen />
+      </Container>
+   
+    </main>
+
+    <Footer />
+     
+    </>
+   
   )
 }
 
